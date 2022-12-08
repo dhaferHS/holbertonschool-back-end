91b64227d55b364fa9064578de9d394deef60942
dhaferHS <dhafer.darkprod@gmail.com>
2022-12-08 17:47:35 +0100

    2-export_to_JSON

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""script to export data in the CSV format"""
+
+
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    userId = sys.argv[1]
+    user = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(userId))
+    name = user.json().get('username')
+
+    todos = user = requests.get('https://jsonplaceholder.typicode.com/todos/')
+    todos = todos.json()
+
+    todoUser = {}
+    tasklist = []
+
+    for task in todos:
+        if task.get('userId') == int(userId):
+            taskDict = {
+                "task": task.get('title'),
+                "completed": task.get('completed'),
+                "username": user.jason().get('username')}
+            taskList.append(taskDict)
+            todoUser[userId] = tasklist
+
+        filename = userId + '.json'
+        with open(filename, mode='w') as f:
+            json.dump(todoUser, f)
