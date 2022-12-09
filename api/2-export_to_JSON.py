9d7f8ee9d4d877ba72b1b0fb8c013aa99ae33206
dhaferHS <dhafer.darkprod@gmail.com>
2022-12-09 09:44:45 +0100

    2-export_to_JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
-"""script to export data in the CSV format"""
-
-
+"""Exports data in the JSON format"""
 import json
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos",
-                         params={"userId": sys.argv[1]}).json()
+
+
+    userId = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = todos.json()
+
+    todoUser = {}
+    taskList = []
 
     for task in todos:
+        if task.get('userId') == int(userId):
+            taskDict = {"task": task.get('title'),
+                        "completed": task.get('completed'),
+                        "username": user.json().get('username')}
+            taskList.append(taskDict)
+    todoUser[userId] = taskList
 
-        taskDict = {
-            "task": task.get('title'),
-            "completed": task.get('completed'),
-            "username": user.get('username')}
-
-    filename = sys.argv[1] + '.json'
-
+    filename = userId + '.json'
     with open(filename, mode='w') as f:
-        json.dump(taskDict, f)
+        json.dump(todoUser, f)
